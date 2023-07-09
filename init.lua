896acc9f8a9ac2b2befd66e73b8e049a5ea8791b
Skoricius <lukaskoric32@gmail.com>
2023-07-09 17:47:28 +0100

    adding clipboard

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
+-- Need to remove xclip and run following
+-- curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.1.1/win32yank-x64.zip
+-- unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+-- chmod +x /tmp/win32yank.exe
+-- sudo mv /tmp/win32yank.exe /usr/local/bin/
+vim.g.clipboard = {
+  name = 'win32yank-wsl',
+  copy = {
+    ['+'] = 'win32yank.exe -i --crlf',
+    ['*'] = 'win32yank.exe -i --crlf',
+  },
+  paste = {
+    ['+'] = 'win32yank.exe -o --lf',
+    ['*'] = 'win32yank.exe -o --lf',
+  },
+  cache_enabled = true,
+}
 return {
   -- Configure AstroNvim updates
   updater = {
