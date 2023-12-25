3128f486fb0dc6c3846eb27540e581a3745840df
Skoricius <lukaskoric32@gmail.com>
2023-12-25 18:39:31 +0100

    adding python formatting to null-ls

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
   -- Set colorscheme to use
   -- colorscheme = "astrodark",
-  colorscheme = "catppuccin",
+  colorscheme = "astrodark",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
@@ -32,6 +32,7 @@ return {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
+          -- "python",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
