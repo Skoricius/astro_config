4f6aafed6f8fd80397592bcaf1496d7b7a080724
Skoricius <lukaskoric32@gmail.com>
2024-02-01 14:02:01 +0000

    adding rainbow csv

--- plugins/user.lua
+++ plugins/user.lua
@@ -23,5 +23,24 @@ return {
     config = function()
         require('nvim-lua/plenary.nvim')
     end,
+  },
+  {
+    'cameron-wags/rainbow_csv.nvim',
+    config = true,
+    ft = {
+        'csv',
+        'tsv',
+        'csv_semicolon',
+        'csv_whitespace',
+        'csv_pipe',
+        'rfc_csv',
+        'rfc_semicolon'
+    },
+    cmd = {
+        'RainbowDelim',
+        'RainbowDelimSimple',
+        'RainbowDelimQuoted',
+        'RainbowMultiDelim'
+    }
   }
 }
